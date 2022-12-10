ea03a1c9207627fffb15dc9e2066a7da517f5925
Ethan Ngawaka <eth.ecn@hotmail.com>
2022-12-10 13:18:50 +1100

    gravity and air resistance

--- main.js
+++ main.js
@@ -1,29 +1,43 @@
 
 class cube{
-    constructor(rect, mass, initialVelocity=[0,0], initialAcceleration=[0,0]){
+    constructor(rect, mass, dragCoefficient=0.01, gravity=true, initialVelocity=[0,0], initialAcceleration=[0,0]){
         this.rect = rect;
         this.m = mass;
         this.vel = initialVelocity;
         this.accel = initialAcceleration;
         this.forces = [];
         this.speed = 5;
+        this.dragCoeff = dragCoefficient;
+        this.gravity = gravity;
+    }
+    resolveForces(){
+        // Force resolution
+        var resultant = [0,0];
+        for(var i of this.forces){
+            resultant[0] += i[0];
+            resultant[1] += i[1];
+        }
+        return resultant;
     }
     physicsUpdate(){
         this.vel[0] += this.accel[0];
         this.vel[1] += this.accel[1];
 
+
         this.rect[0] += this.vel[0];
         this.rect[1] += this.vel[1];
 
-        var resultant = [0,0];
-        // Force resolution
-        for(var i of this.forces){
-            resultant[0] += i[0];
-            resultant[1] += i[1];
+        // air resistance
+        this.addForce([-(Math.sign(this.vel[0]))*(this.vel[0]**2)*this.dragCoeff,-(Math.sign(this.vel[1]))*(this.vel[1]**2)*this.dragCoeff])
+        //
+        if(this.gravity){
+            this.addForce([0,9.8]);
         }
 
+        var resultant = this.resolveForces();
+        
         this.accel = [resultant[0]/this.m, resultant[1]/this.m];
-        this.forces = []; // not sure if i do this
+        this.forces = [];
 
     }
     addForce(forceVector){
